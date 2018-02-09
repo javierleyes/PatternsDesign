3280f7ee0f71bfed3d9f57581776cd4371c1aa4e
= <=>
2018-02-09 18:35:49 -0300

    Patron Singleton

---
+++ PatronSingleton/PatronSingleton/Institucion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace PatronSingleton
+{
+    public class Institucion
+    {
+        private Institucion() { }
+
+        //Thread Safe
+        private static Institucion instance = new Institucion();
+
+        public string Nombre { get; set; }
+
+        public static Institucion GetInstance()
+        {
+            return instance;
+        }
+    }
+}
---
+++ PatronSingleton/PatronSingleton/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PatronSingleton
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Institucion instancia1 = Institucion.GetInstance();
+            instancia1.Nombre = "Maria";
+            Console.WriteLine("Instancia 1: " + instancia1.Nombre);
+
+            Institucion instancia2 = Institucion.GetInstance();
+            instancia2.Nombre = "Jose";
+            Console.WriteLine("Instancia 2: " + instancia2.Nombre);
+
+            Console.WriteLine("Nombre instancia1: " + instancia1.Nombre);
+        }
+    }
+}
